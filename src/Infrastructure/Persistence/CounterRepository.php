2e3099af9604e757ba48b275688b0808cf1bb194
Vinicius Ceratti <v.ceratti@gmail.com>
2023-07-12 18:40:18 +0200

    feat(database-entities): TEMP

--- src/Infrastructure/Persistence/CounterRepository.php
+++ src/Infrastructure/Persistence/CounterRepository.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace LogAggregator\Infrastructure\Persistence;
 
+use LogAggregator\Application\DTO\CounterFilterDTO;
+use LogAggregator\Domain\Counter;
+
 interface CounterRepository
 {
+    public function countRequestLogs(CounterFilterDTO $counterFilterDTO): Counter;
 }
--- src/Infrastructure/Persistence/Database/Repository/CounterRepository.php
+++ src/Infrastructure/Persistence/Database/Repository/CounterRepository.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace LogAggregator\Infrastructure\Persistence\Database\Repository;
 
+use LogAggregator\Application\DTO\CounterFilterDTO;
+use LogAggregator\Domain\Counter;
 use LogAggregator\Infrastructure\Persistence\CounterRepository as CounterRepositoryInterface;
 
 class CounterRepository implements CounterRepositoryInterface
 {
+    public function countRequestLogs(CounterFilterDTO $counterFilterDTO): Counter
+    {
+        return new Counter(0);
+    }
 }
